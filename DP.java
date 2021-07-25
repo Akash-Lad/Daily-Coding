c3d6f6e6a1e41e9c9c1cb8bb844a9cd8d7a51358
Akash-Lad <akashlad4000@gmail.com>
2021-07-25 11:54:26 +0530

    update

--- DP.java
+++ DP.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+//import java.util.*;
 
 public class DP {
 
@@ -187,6 +187,198 @@ public class DP {
 
     }
 
+    public static int countSubsetSumWithGivenDifference(int arr[], int diff) {
+
+        int sumofarray = 0;
+
+        for (int x : arr)
+            sumofarray += x;
+
+        int sum = (diff + sumofarray) / 2;
+
+        return countOfSubsetOfGivenSum(arr, sum);
+    }
+
+    public int countOfSubsetSumWithGivenDifference(int[] arr, int sum) {
+
+        int n = arr.length;
+
+        int[][] dp = new int[n + 1][sum + 1];
+
+        for (int i = 0; i < n + 1; i++)
+            dp[i][0] = 1;
+
+        for (int j = 1; j < sum + 1; j++)
+            dp[0][j] = 0;
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+
+                if (arr[i - 1] == 0)
+                    dp[i][j] = dp[i - 1][j];
+                else if (arr[i - 1] > j)
+                    dp[i][j] = dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j - arr[i - 1]] + dp[i - 1][j];
+
+            }
+        }
+
+        return dp[n][sum];
+
+    }
+
+    public int findTargetSumWays(int[] arr, int target) {
+
+        int sumofarray = 0, count = 0;
+
+        for (int x : arr) {
+
+            sumofarray += x;
+
+            // Edge-case-1
+            if (x == 0)
+                count = count + 1;
+
+        }
+
+        // Edge-case-2
+        if (target > sumofarray)
+            return 0;
+
+        // Edge-case-3
+        if ((target + sumofarray) % 2 != 0)
+            return 0;
+
+        int sum = (target + sumofarray) / 2;
+
+        int result = countOfSubsetSumWithGivenDifference(arr, sum);
+
+        return (int) Math.pow(2, count) * result;
+
+    }
+
+    // Rod Cutting Problem
+
+    public static int cutRod(int n, int[] price) {
+        /*
+         * length[] price[] n
+         */
+
+        int[] length = new int[n];
+        int size = price.length;
+
+        for (int i = 0; i < n; i++)
+            length[i] = i + 1;
+
+        int[][] dp = new int[n + 1][size + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int j = 1; j < size + 1; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < size + 1; j++) {
+
+                if (length[i - 1] <= j) {
+                    dp[i][j] = Math.max(price[i - 1] + dp[i][j - length[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][size];
+    }
+
+    public static int sticklerThief(int arr[], int n) {
+
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return arr[0];
+        if (n == 2)
+            return Math.max(arr[0], arr[1]);
+
+        int[] dp = new int[n];
+        dp[0] = arr[0];
+        dp[1] = Math.max(arr[0], arr[1]);
+
+        for (int i = 2; i < n; i++)
+            dp[i] = Math.max(arr[i] + dp[i - 2], dp[i - 1]);
+
+        return dp[n - 1];
+    }
+
+    // Coin change - I (Maximum number of coins)
+
+    public static int coinChange_01(int[] coin, int sum) {
+
+        int n = coin.length;
+        int m = sum;
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 0; i < n + 1; i++)
+            dp[i][0] = 1;
+
+        for (int j = 1; j < m + 1; j++)
+            dp[0][j] = 0;
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+
+                if (coin[i - 1] <= j) {
+                    dp[i][j] = dp[i][j - coin[i - 1]] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+
+            }
+        }
+
+        return dp[n][m];
+
+    }
+
+    // Coin change - II (Minimum number of coins)
+
+    public static int coinChange_02(int[] coin, int sum) {
+
+        int n = coin.length;
+        int m = sum;
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i < n + 1; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int j = 0; j < m + 1; j++) {
+            dp[0][j] = Integer.MAX_VALUE;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+
+                if (coin[i - 1] <= j) {
+                    dp[i][j] = Math.min(1 + dp[i][j - coin[i - 1]], dp[i - 1][j]);
+
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+
+            }
+        }
+
+        return dp[n][m];
+
+    }
+
     // Greedy min number of coins
 
     public static int coinNumber(int val) {
@@ -211,9 +403,10 @@ public class DP {
 
     public static void main(String args[]) {
 
-        int arr[] = { 1, 2 };
-        int k = 1;
-        System.out.println(subsetSumWithMinimumDifference(arr, k));
+        int arr[] = new int[] { 1, 5, 8, 9, 10, 17, 17, 20 };
+        int size = arr.length;
+
+        System.out.println(cutRod(size, arr));
 
     }
 
--- README.md
+++ README.md
@@ -1,7 +1,30 @@
 # Dynamic Programming
 
+## 0-1 Knapsack type
+
 - 01 Knapsack Problem
 - Subset sum problem
 - Count of subset of given sum
-- Partition of K equal subset sum
-- Subset sum with minimum difference
+- Partition equal subset sum
+- Subset sum with minimum
+- Count the number of subset with given minimim difference
+- Target Sum
+- Stickler Thief
+
+## Unbounded Knapsack type
+
+- Rod Cutting Problem
+- Coin Change - Minimum number of coins
+- Coin Change - Number of ways
+- Longest Common Subsequence
+- Longest Common Substring
+- Print LCS
+- Minimum number of insertions and deletions to convert "a" -> "b"
+- Largest Repeating Subsequence
+- Length of largest subsequence of "a" which is substring of "b"
+- Subsequence Pattern Matching
+- Count how many times "a" appears as subsequence in "b"
+- Largest palindromic subsequence
+- Count of palindromic substring
+- Minimum number of deletion in string to make it a palindrome
+- Minimum number of insertion in string to make it a palindrome
